91833e78dd0319c8bfb084b978f8c32c1a6f081e
Runcy Oommen <runcy.oommen@gmail.com>
2017-07-17 18:31:47 +0530

    New updated query (granular) and some links for reference. Lambda package still pending

--- setup/select_race_results.py
+++ setup/select_race_results.py
@@ -1,5 +1,9 @@
 import MySQLdb
+import logging
 from connection_string import cnx_str
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 def select_race_results():
@@ -7,24 +11,23 @@ def select_race_results():
 
     try:
         cursor = connection.cursor()
-        cursor.execute('SELECT * from race_results;')
-
+        cursor.execute('SELECT driver_name from race_results where race_results.year = "2017" AND '
+                       'race_results.circuit_name = "Spain" AND race_results.position = "1";')
         row = cursor.fetchone()
+        print(row)
 
-        print("Retrieving data from race_results")
-        while row is not None:
-            print(row)
-            row = cursor.fetchone()
-    except:
+        # cursor.execute('SELECT * from race_results;')
+        # row = cursor.fetchone()
+        # print("Retrieving data from race_results")
+        # while row is not None:
+        #     print(row)
+        #     row = cursor.fetchone()
+    except IndexError:
         print('Unexpected error occurred')
     finally:
         cursor.close()
         connection.close()
 
 
-def main():
-    select_race_results()
-
-
 if __name__ == '__main__':
-    main()
+    select_race_results()
